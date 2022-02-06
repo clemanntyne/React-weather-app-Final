1fd8bc42db14d0488b8191b6d7b55263d071b90c
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-02-06 10:31:58 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Stephanie Clemann and is{" "}
-        <a
-          href="https://github.com/clemanntyne/React-weather-app-Final"
-          traget="_blank"
-        >
-          Open Source
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Stephanie Clemann and is{" "}
+          <a
+            href="https://github.com/clemanntyne/React-weather-app-Final"
+            traget="_blank"
+          >
+            Open Source
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
