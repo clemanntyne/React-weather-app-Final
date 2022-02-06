c83f673e0fa0e82c026c9dbbfc1b43bbfad75a21
clemanntyne <92274079+clemanntyne@users.noreply.github.com>
2022-02-06 10:19:33 -0800

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Stephanie Clemann and is{" "}
+        <a
+          href="https://github.com/clemanntyne/React-weather-app-Final"
+          traget="_blank"
+        >
+          Open Source
+        </a>{" "}
+        on GitHub
+      </footer>
+    </div>
+  );
 }
